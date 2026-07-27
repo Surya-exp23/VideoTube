3afa959d0d3ae702d5bcb0ade84055c22195e3ed
Surendra Singh <surendrasinghrathore13057@gmail.com>
2026-07-27 18:47:37 +0530

    connected the mongodb

--- src/app.js
+++ src/app.js
@@ -7,8 +7,14 @@ app.use(
     cors({
         origin: process.env.CORS_ORIGIN,
         credentials: true
-    })
+    }) 
 )
+
+// common middleware
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true,limit: "16kb"}))
+app.use(express.static("public"))
+
 
 
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import {app} from './app.js';
 import dotenv from 'dotenv'
+import connectDB from './db/index.js';
+
+import dns from "dns";
+
+dns.setServers(["1.1.1.1","8.8.8.8"]);
 
 dotenv.config({
     path: "./.env"
 })
 
 const PORT = process.env.PORT || 5000
-app.listen(5000,()=>{
-    console.log(`console is running on port: ${PORT}`)
+
+connectDB().
+then(()=>{
+    app.listen(5000,()=>{
+        console.log(`console is running on port: ${PORT}`)
+    })
 })
+.catch((err)=>{
+    console.log("mongodb is not working error", err)
+})
+
+
+
 
