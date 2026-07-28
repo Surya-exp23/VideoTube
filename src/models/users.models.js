fe42678b735fc136da11a3e334c951e0e79684be
Surendra Singh <surendrasinghrathore13057@gmail.com>
2026-07-28 08:10:50 +0530

    modelled userschema

---
+++ src/models/users.models.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },fullname:{
+            type: String,
+            required: true,
+            trim: true,
+        },avatar:{
+            type: String, //cloudinary link - we will learn about it in future
+            required: true
+        },coverpage:{
+            type: String, 
+        },watchHistory:[
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password:{
+            type: String,
+            required: [true, "password is required"]
+        },refreshToken:{
+            type: String 
+        }
+    },
+    { timestamps: true}
+)
+
+
+export const User = mongoose.model("User",userSchema) 
