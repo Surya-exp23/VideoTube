84fcf25638baf9230b3e0a4a4df44c143ba6e104
Surendra Singh <surendrasinghrathore13057@gmail.com>
2026-09-16 22:08:26 +0530

    fix import path for User model in auth middleware

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
-import { User } from '../models/user.model.js';
-import {ApiError} from '../utils/apiError.js';
+import { User } from '../models/users.models.js';
+import {ApiError} from '../utils/ApiError.js';
 import {asyncHandler} from '../utils/asyncHandler.js';
 
 export const verifyJWT = asyncHandler(async (req, _, next)=>{
